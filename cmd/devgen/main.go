141c87adda7a83a1126306dde5facc52e61a6f31
saito <birdyfj@gmail.com>
2026-01-07 17:14:06 +0900

    chore: add version

--- cmd/devgen/main.go
+++ cmd/devgen/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	_ "embed"
+
 	"github.com/spf13/cobra"
 
 	"github.com/xoctopus/devx/internal/devx"
@@ -12,11 +14,17 @@ var (
 	Version   string
 	CommitID  string
 	BuildTime string
+
+	//go:embed version
+	version string
 )
 
 var CmdVersion = &cobra.Command{
 	Use: "version",
 	Run: func(cmd *cobra.Command, args []string) {
+		if Name == "" {
+			cmd.Println(version)
+		}
 		cmd.Printf("%s:%s@%s#%s_%s\n", Name, Branch, Version, CommitID, BuildTime)
 	},
 	Short: "print the version of DevX/devgen",
